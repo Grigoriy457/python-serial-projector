4c2fc97e4181ff22de1e1474b8261abd69612409
Grigoriy457 <grigori.p.vlasov@gmail.com>
2024-06-26 18:50:13 +0300

    Connect to github

---
+++ app.py
@@ -0,0 +1,116 @@
+import eel
+import serial
+from serial.tools import list_ports
+import threading
+import json
+import os
+import requests
+import webbrowser
+
+
+REPOS_OWNER = "Grigoriy457"
+REPOS_NAME = "python-serial-projector"
+VERSION = 1.1
+event = None
+
+
+
+@eel.expose
+def check_for_update():
+	response = requests.get(f"https://api.github.com/repos/{REPOS_OWNER}/{REPOS_NAME}/tags")
+	if response.status_code == 200:
+		last_ver = float(response.json()[0]["name"][1:])
+		print("Latest version:", last_ver)
+		return (last_ver > VERSION, last_ver)
+	return (False, VERSION)
+
+
+@eel.expose
+def open_last_version(version):
+	webbrowser.open_new(f"https://github.com/{REPOS_OWNER}/{REPOS_NAME}/releases/tag/v{version}")
+
+
+@eel.expose
+def get_com_list():
+	com_list = [str(i) for i in list_ports.comports()]
+	return com_list
+
+
+def serial_reader(port, bit_rate, data_bits, parity_bit, stop_bits, event):
+	bytesize = serial.EIGHTBITS
+	if data_bits == "7":
+		bytesize = serial.SEVENBITS
+
+	parity = serial.PARITY_NONE
+	if parity_bit == "odd":
+		parity = serial.PARITY_ODD
+	elif parity_bit == "even":
+		parity = serial.PARITY_EVEN
+
+	stopbits = serial.STOPBITS_ONE
+	if stop_bits == "two":
+		stopbits = serial.STOPBITS_TWO
+
+	try:
+		s = serial.Serial(port=port.split(" - ")[0], baudrate=int(bit_rate), parity=parity, stopbits=stopbits, bytesize=bytesize, timeout=0)
+	except serial.serialutil.SerialException:
+		eel.sleep(0.05)
+		eel.cant_connect()
+		return
+	while True:
+		try:
+			string = ""
+			while True:
+				if event.is_set():
+					s.close()
+					return
+				if s.in_waiting != 0:
+					symbol = s.readline()
+					try:
+						symbol = symbol.decode()
+						string += symbol
+						if symbol.endswith("\n"):
+							break
+					except UnicodeDecodeError:
+						print("[ERROR]:", symbol)
+
+			string = string.strip()
+			if string != "":
+				print("[SERIAL]:", string)
+				eel.set_text(string)
+		except serial.serialutil.SerialException:
+			eel.device_lost()
+			break
+
+
+
+@eel.expose
+def py_connect(port, bit_rate, data_bits, parity_bit, stop_bits):
+	global event
+
+	if port == "":
+		return
+
+	event = threading.Event()
+	threading.Thread(target=serial_reader, args=(port, bit_rate, data_bits, parity_bit, stop_bits, event)).start()
+	return 1
+
+
+@eel.expose
+def py_disconnect():
+	global event
+
+	if event is not None:
+		event.set()
+		eel.sleep(0.05)
+	return 1
+
+
+def close_callback(route, websockets):
+	if not websockets:
+		exit()
+
+
+if __name__ == '__main__':
+	eel.init('web')
+	eel.start('main.html', size=(800, 500), close_callback=close_callback, port=8080)
---
+++ web/js/script.js
@@ -0,0 +1,142 @@
+var is_clicked_in_menu = false;
+
+
+function set_settings() {
+     var elem = document.getElementById("wrap");
+     elem.style.fontSize = settings["font-size"];
+     elem.style.color = settings["text-color"];
+     elem.style.backgroundColor = settings["bg-color"];
+}
+
+function reset_settings() {
+     settings = Object.assign({}, main_settings);
+     document.getElementById("font-size").value = settings["font-size"].slice(0, -2);
+     document.getElementById("text-color").value = settings["text-color"];
+     document.getElementById("bg-color").value = settings["bg-color"];
+     set_settings();
+}
+
+function clicked_on_menu() {
+     is_clicked_in_menu = true;
+}
+
+function open_close_modal_settings(btn) {
+     if (btn == null) {
+          console.log(is_clicked_in_menu)
+          if (is_clicked_in_menu) {
+               is_clicked_in_menu = false;
+               return;
+          }
+          for (var i of ["connection", "settings", "about"]) {
+               document.getElementById(i).style.display = "none";
+          }
+          return;
+     }
+     is_clicked_in_menu = true;
+     var name = btn.getAttribute("btn-name");
+     
+     var now_selected = null;
+     for (var i of ["connection", "settings", "about"]) {
+          if (document.getElementById(i).style.display == "block") {
+               now_selected = i;
+          }
+          document.getElementById(i).style.display = "none";
+     }
+
+     if (now_selected == name) {
+          document.getElementById(now_selected).style.display = "none";
+          return;
+     }
+
+     if (name == "fullscreen") {
+
+     } else if (name == "connection") {
+          check_ports();
+          document.getElementById("connection").style.display = "block";
+     } else if (name == "settings") {
+          document.getElementById("settings").style.display = "block";
+     } else if (name == "about") {
+          document.getElementById("about").style.display = "block";
+     }
+}
+
+function check_ports() {
+     eel.get_com_list()(function (ports) {
+          var ports_elem = document.getElementById("port");
+          ports_elem.disabled = false;
+     
+          for (var i of ports_elem.getElementsByTagName("option")) { i.remove(); }
+          if (ports.length == 0) {
+               var option = document.createElement("option")
+               option.text = "No devices";
+               option.value = "";
+               ports_elem.add(option, null);
+               ports_elem.disabled = true;
+          } else {
+               for (var i of ports) {
+                    var option = document.createElement("option")
+                    option.text = i;
+                    option.value = i;
+                    ports_elem.add(option, null);
+               }
+          }
+     });
+}
+
+function connect() {
+     port = document.getElementById("port").value;
+     bitrate = document.getElementById("bitrate").value;
+     dataBits = document.getElementById("dataBits").value;
+     parityBit = document.getElementById("parityBit").value;
+     stopBits = document.getElementById("stopBits").value;
+     eel.py_connect(port, bitrate, dataBits, parityBit, stopBits)(function(ret) {
+          if (ret == 1) {
+               document.getElementById("connect").style.display = "none";
+               document.getElementById("stop-connection").style.display = "block";
+               for (var i of ["port", "bitrate", "dataBits", "parityBit", "stopBits"]) {
+                    document.getElementById(i).disabled = true;
+               }
+          }
+     });
+}
+
+function disconnect() {
+     eel.py_disconnect()(function(ret) {
+          if (ret == 1) {
+               document.getElementById("connect").style.display = "block";
+               document.getElementById("stop-connection").style.display = "none";
+               document.getElementById("wrap").innerHTML = "<h1>disconnected</h1>"
+               for (var i of ["port", "bitrate", "dataBits", "parityBit", "stopBits"]) {
+                    document.getElementById(i).disabled = false;
+               }
+               check_ports();
+          }
+     });
+}
+
+eel.expose(device_lost);
+function device_lost() {
+     document.getElementById("connect").style.display = "block";
+     document.getElementById("stop-connection").style.display = "none";
+     document.getElementById("wrap").innerHTML = "<h1>device_lost</h1>";
+     for (var i of ["port", "bitrate", "dataBits", "parityBit", "stopBits"]) {
+          document.getElementById(i).disabled = false;
+     }
+     check_ports();
+}
+
+eel.expose(cant_connect);
+function cant_connect(com_port) {
+     document.getElementById("connect").style.display = "block";
+     document.getElementById("stop-connection").style.display = "none";
+     document.getElementById("wrap").innerHTML = `<h1>Connection failed</h1><h6>Can't open serial port ${com_port}.</h6><h6>Possibly it is already in use by another application.</h6>`;
+     for (var i of ["port", "bitrate", "dataBits", "parityBit", "stopBits"]) {
+          document.getElementById(i).disabled = false;
+     }
+     check_ports();
+}
+
+eel.expose(set_text);
+function set_text(string) {
+     document.getElementById("wrap").innerHTML = `<h1>${string}</h1>`;
+}
